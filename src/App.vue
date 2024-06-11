30efbb2dd061d94c20827515f635947078e71959
GfCarvalho77 <127401543+GfCarvalho77@users.noreply.github.com>
2024-06-11 15:26:01 -0300

    atualização de cores

--- src/App.vue
+++ src/App.vue
@@ -75,8 +75,8 @@ export default {
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #f30a0a;
+  margin-top: 200px;
 }
 </style>
 
@@ -88,7 +88,7 @@ export default {
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   text-align: center;
-  color: #2c3e50;
+  color: #28673f;
   margin-top: 60px;
 }
 </style>
